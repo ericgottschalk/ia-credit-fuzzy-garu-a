6142058c28d442b69f68245d79a915b3377b6558
Eric Gottschalk <ericng04@hotmail.com>
2018-10-01 18:42:35 -0300

    feat(fuzzy)

---
+++ ENG.Fuzzy/Functions/Triangular.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ENG.Fuzzy.Functions
+{
+    public class Triangular : Constructor
+    {
+        public Triangular(level level, float pointA, float pointB, float pointC)
+            : base(level)
+        {
+            this.Begin = pointA;
+            this.End = pointC;
+            this.PointA = pointA;
+            this.PointB = pointB;
+            this.PointC = pointC;
+        }
+
+        public override float Value(float x)
+            => Math.Max(Math.Min((x - this.PointA) / (this.PointB - this.PointA), (this.PointC - x) / (this.PointC - this.PointB)), 0);
+    }
+}
